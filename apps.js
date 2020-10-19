0c9e6f1ac13d174ae55f6a304600f95597d6694a
Sairoden Gandarosa <72735313+Sairoden@users.noreply.github.com>
2020-10-19 20:15:36 +0800

    Update apps.js
    Fixing the connection of ejs and js

--- apps.js
+++ apps.js
@@ -15,13 +15,30 @@ const connection = mysql.createConnection({
 
 // Connecting database
 connection.connect(function (error) {
-    if (!!error) console.log(error);
-    else console.log("Database Connected!");
+  if (!!error) console.log(error);
+  else console.log("Database Connected!");
+});
+
+//set views file
+app.set("views", path.join(__dirname, "views"));
+
+//set view engine
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/items", (req, res) => {
+  let sql = "SELECT * FROM items";
+  let query = connection.query(sql, (err, rows) => {
+    if (err) throw err;
+    res.render("index", {
+      title: "Inventory",
+      items: rows
+    });
   });
-  
-  //set views file
-  app.set("views", path.join(__dirname, "views"));
-  //set view engine
-  app.set("view engine", "ejs");
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
+});
+
+// Server Listening
+app.listen(8700, () => {
+  console.log("Server is running at port 8700");
+});
